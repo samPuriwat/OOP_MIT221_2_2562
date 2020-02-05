4f686386325af565aec5464d1cdcc3f6ffaa20f6
samPuriwat <sam.puriwat@gmail.com>
2020-02-05 11:49:42 +0700

    Lab 8: inheritance

---
+++ src/lab8/Person.java
@@ -0,0 +1,33 @@
+package lab8;
+
+public class Person {
+    private String id;
+    private String name;
+
+    //constructor
+
+    //public Person() {
+   // }
+    public Person(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    //getter and setter
+    public String getId() {
+        return id;
+    }
+    protected String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    //toString()\
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/lab8/PersonApp.java
@@ -0,0 +1,20 @@
+package lab8;
+
+public class PersonApp {
+
+    public static void main(String[] args) {
+        //create object
+        Student std1 = new Student("1111111111111",
+                "Puriwat Lertkrai",
+                "STD001",
+                "Information Technology");
+        Person p1 = new Person("222222222222222",
+                "Piyapong Senanus");
+
+        System.out.println(std1.getName());
+        System.out.println(p1.getName());
+
+
+
+    }
+}
